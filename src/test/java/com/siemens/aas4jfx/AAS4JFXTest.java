b6370fadcabb4f4879a0fa2f69276e9d3a41280b
danielpeintner <daniel.peintner@gmail.com>
2024-04-24 11:18:13 +0200

    chore: add AAS tests and dependency

--- src/test/java/com/siemens/aas4jfx/AAS4JFXTest.java
+++ src/test/java/com/siemens/aas4jfx/AAS4JFXTest.java
@@ -1,14 +1,28 @@
 package com.siemens.aas4jfx;
 
+import org.eclipse.digitaltwin.aas4j.v3.dataformat.core.DeserializationException;
+import org.eclipse.digitaltwin.aas4j.v3.dataformat.xml.XmlDeserializer;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class AAS4JFXTest {
 
     @Test
-    public void test1() {
-        assertEquals(1, 1);
+    public void testOk1() throws IOException, DeserializationException {
+        new XmlDeserializer().read(AAS4JFXTest.class.getResource("/AAS_Template_for_AID.aas.xml").openStream());
+    }
+
+    @Test
+    public void testFail1() {
+        try {
+            new XmlDeserializer().read(AAS4JFXTest.class.getResource("/AAS_Template_for_AID_Invalid.aas.xml").openStream());
+            fail("Expected to fail");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
 }
